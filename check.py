0297a66a6216cb03537cd627b237f25ebfed59ef
Samantha St. Clair <113750273+salmon82@users.noreply.github.com>
2024-09-05 11:45:59 -0700

    Update check.py

--- check.py
+++ check.py
@@ -98,7 +98,9 @@ st.title("Display of Your Video")
 
 
 st.markdown(f"""
-<iframe width="560" height="315" src="{url}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <div style="display: flex; justify-content: center;">
+        <iframe width="560" height="315" src="{url}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
 """, unsafe_allow_html=True)
 
 # Pass result to a dataframe
